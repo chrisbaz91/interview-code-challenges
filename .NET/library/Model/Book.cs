316ae5f29c5a152cc26e0a342201a71764639776
chrisbaz <chrisbaz91@hotmail.co.uk>
2025-04-16 00:38:49 +0100

    Book regular constructor + parameterless constructor to help mapping

--- .NET/library/Model/Book.cs
+++ .NET/library/Model/Book.cs
@@ -2,6 +2,29 @@
 {
     public class Book
     {
+        public Book()
+        {
+
+        }
+
+        public Book(string name, Author author, BookFormat format, string isbn)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException($"'{nameof(name)}' cannot be null or whitespace.", nameof(name));
+            }
+
+            if (string.IsNullOrWhiteSpace(isbn))
+            {
+                throw new ArgumentException($"'{nameof(isbn)}' cannot be null or whitespace.", nameof(isbn));
+            }
+
+            Name = name;
+            ISBN = isbn;
+            Author = author ?? throw new ArgumentNullException(nameof(author));
+            Format = format;
+        }
+
         public Guid Id { get; set; }
         public string Name { get; set; }
         public Author Author { get; set; }
