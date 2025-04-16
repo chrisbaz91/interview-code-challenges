169dc69ea0280d982a6f5ee1735afdecc9c35ab3
chrisbaz <chrisbaz91@hotmail.co.uk>
2025-04-16 01:18:07 +0100

    Additional SeedData

--- .NET/library/SeedData.cs
+++ .NET/library/SeedData.cs
@@ -10,6 +10,10 @@ namespace OneBeyondApi
             var ernestMonkjack = new Author("Ernest Monkjack");
             var sarahKennedy = new Author("Sarah Kennedy");
             var margaretJones = new Author("Margaret Jones");
+            var markMillar = new Author("Mark Millar");
+            var brianMichaelBendis = new Author("Brian Michael Bendis");
+            var jMStraczynski = new Author("J M Straczynski");
+            var johnathanHickman = new Author("Johnathan Hickman");
 
             var clayBook = new Book
             (
@@ -35,6 +39,38 @@ namespace OneBeyondApi
                 "3134324111"
             );
 
+            var civilWarBook = new Book
+            (
+                "Marvel Civil War",
+                markMillar,
+                BookFormat.GraphicNovel,
+                "9781905239603"
+            );
+
+            var avxBook = new Book
+            (
+                "Avengers vs. X-Men",
+                brianMichaelBendis,
+                BookFormat.GraphicNovel,
+                "9780785138938"
+            );
+
+            var spiderManBook = new Book
+            (
+                "Spider-Man Ultimate Collection Vol. 1",
+                jMStraczynski,
+                BookFormat.GraphicNovel,
+                "9780785163176"
+            );
+
+            var infinityBook = new Book
+            (
+                "Marvel Infinity",
+                johnathanHickman,
+                BookFormat.GraphicNovel,
+                "9780785184232"
+            );
+
             var daveSmith = new Borrower
             (
                 "Dave Smith",
@@ -46,6 +82,15 @@ namespace OneBeyondApi
                 "Liana James",
                 "liana@gmail.com"
             );
+
+            var chrisBarrett = new Borrower
+            (
+                "Chris Barrett",
+                "cpbarrett91@gmail.com"
+            )
+            {
+                FinesOwed = 2.25
+            };
 
             var bookOnLoanUntilToday = new BookStock(clayBook)
             {
@@ -71,23 +116,67 @@ namespace OneBeyondApi
                 LoanEndDate = null
             };
 
+            var civilWarBookStock = new BookStock(civilWarBook)
+            {
+                OnLoanTo = chrisBarrett,
+                LoanEndDate = DateTime.Now.Date.AddDays(-7)
+            };
+
+            var avxBookStock = new BookStock(avxBook)
+            {
+                OnLoanTo = null,
+                LoanEndDate = null
+            };
+
+            var avxBookStock2 = new BookStock(avxBook)
+            {
+                OnLoanTo = chrisBarrett,
+                LoanEndDate = DateTime.Now.Date.AddDays(-7)
+            };
+
+            var spiderManBookStock = new BookStock(spiderManBook)
+            {
+                OnLoanTo = null,
+                LoanEndDate = null
+            };
+
+            var infinityBookStock = new BookStock(infinityBook)
+            {
+                OnLoanTo = null,
+                LoanEndDate = null
+            };
+
             using var context = new LibraryContext();
 
             context.Authors.Add(ernestMonkjack);
             context.Authors.Add(sarahKennedy);
             context.Authors.Add(margaretJones);
+            context.Authors.Add(markMillar);
+            context.Authors.Add(brianMichaelBendis);
+            context.Authors.Add(jMStraczynski);
+            context.Authors.Add(johnathanHickman);
 
             context.Books.Add(clayBook);
             context.Books.Add(agileBook);
             context.Books.Add(rustBook);
+            context.Books.Add(civilWarBook);
+            context.Books.Add(avxBook);
+            context.Books.Add(spiderManBook);
+            context.Books.Add(infinityBook);
 
             context.Borrowers.Add(daveSmith);
             context.Borrowers.Add(lianaJames);
+            context.Borrowers.Add(chrisBarrett);
 
             context.Catalogue.Add(bookOnLoanUntilToday);
             context.Catalogue.Add(bookNotOnLoan);
             context.Catalogue.Add(bookOnLoanUntilNextWeek);
             context.Catalogue.Add(rustBookStock);
+            context.Catalogue.Add(civilWarBookStock);
+            context.Catalogue.Add(avxBookStock);
+            context.Catalogue.Add(avxBookStock2);
+            context.Catalogue.Add(spiderManBookStock);
+            context.Catalogue.Add(infinityBookStock);
 
             await context.SaveChangesAsync();
         }
